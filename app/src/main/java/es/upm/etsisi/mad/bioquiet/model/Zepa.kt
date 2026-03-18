b2be927624c34e26a3223374a0c370f24dffc594
Stefan Oshchypok <stefan.oshchypok@alumnos.upm.es>
2026-03-18 12:27:30 +0100

    feat: zepa model

---
+++ app/src/main/java/es/upm/etsisi/mad/bioquiet/model/Zepa.kt
@@ -0,0 +1,62 @@
+package es.upm.etsisi.mad.bioquiet.model
+
+data class Zepa(
+    val id: String,
+    val name: String,
+    val noiseThresholds: NoiseThresholds,
+    val areaHa: Double?,
+    val dateSpa: String?,
+    val spaLegalRef: String?,
+    val description: String?,
+    val quality: String?,
+    val habitats: List<Habitat>,
+    val species: List<Species>,
+    val impacts: List<Impact>,
+    val management: List<Management>,
+    val geometry: GeoJsonGeometry
+)
+
+data class NoiseThresholds(
+    val dbSafe: Int,
+    val dbWarning: Int
+)
+
+data class Habitat(
+    val code: String?,
+    val description: String?,
+    val priority: Boolean?,
+    val coverHa: Double?,
+    val representativity: String?,
+    val conservation: String?,
+    val globalAssessment: String?
+)
+
+data class Species(
+    val code: String?,
+    val name: String?,
+    val group: String?,
+    val populationType: String?,
+    val abundance: String?,
+    val conservation: String?,
+    val global: String?
+)
+
+data class Impact(
+    val code: String?,
+    val description: String?,
+    val intensity: String?,
+    val occurrence: String?,
+    val type: String?
+)
+
+data class Management(
+    val orgName: String?,
+    val orgEmail: String?,
+    val planUrl: String?,
+    val measures: String?
+)
+
+data class GeoJsonGeometry(
+    val type: String,
+    val coordinates: Any
+)
